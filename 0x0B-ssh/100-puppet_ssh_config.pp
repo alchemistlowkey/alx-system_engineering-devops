9a0df89ef5ac5ee3ac1fdcab8bbc4757d0c85f9f
alchemistlowkey <alchemistlowkey@gmail.com>
2023-11-26 13:19:40 -0800

    config file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,9 @@
 # Puppet to make changes to our configuration file
-exec { 'file':
-  path    => 'usr/bin:/bin',
-  command => 'file "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
-  returns => [0,1],
+exec { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  command => 'PasswordAuthentication no',
+}
+exec { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  command => 'IdentityFile ~/.ssh/holberton',
 }
