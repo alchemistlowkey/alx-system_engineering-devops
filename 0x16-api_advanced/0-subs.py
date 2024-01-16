35706c5f22df86f5c8a783bab1041cce684686f6
alchemistlowkey <alchemistlowkey@gmail.com>
2024-01-16 13:27:44 -0800

    reddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,20 +2,18 @@
 """
 A function that queries the Reddit API and returns the number of subscribers
 """
-import json
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """
-    This returns the number of subscribers
-    """
+    '''Returns the number of subscribers'''
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "MyRedditBot"}
+
     response = requests.get(url, headers=headers)
+
     if response.status_code == 200:
-        reddits = response.json()
-        count = reddits["data"]["subscribers"]
-        return count
+        data = response.json()
+        subscribers_count = data["data"]["subscribers"]
+        return subscribers_count
     else:
         return 0
