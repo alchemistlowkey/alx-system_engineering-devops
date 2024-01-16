f0a0a8d9af2a1cbba3db4fa54e3fa0fda2aa8bc7
alchemistlowkey <alchemistlowkey@gmail.com>
2024-01-16 13:37:59 -0800

    redirect

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,11 +13,9 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "My-Reddit-header"}
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        reddits = response.json()
-        count = data["data"]["subscribers"]
-        return count
+        return response.json()["data"]["subscribers"]
     else:
         return 0
